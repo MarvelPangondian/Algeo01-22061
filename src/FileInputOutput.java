f15cd7b1387777dde60f39495d6ec45e1c992662
Marvel Pangondian <marvel.pangondian01@gmail.com>
2023-09-28 09:12:43 +0700

    add FileInputOutput.java

---
+++ src/FileInputOutput.java
@@ -0,0 +1,11 @@
+
+public class FileInputOutput {
+	String path;
+	public FileInputOutput(String Path) {
+		// TODO Auto-generated constructor stub
+		this.path = path;
+	}
+	public static void what() {
+		System.out.printf("Why hello");
+	}
+}
