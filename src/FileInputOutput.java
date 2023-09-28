f0c98398311d0ac16b72bea971e68b3a3ddbd719
Marvel Pangondian <marvel.pangondian01@gmail.com>
2023-09-28 13:40:42 +0700

    add readFileMatrix method

--- src/FileInputOutput.java
+++ src/FileInputOutput.java
@@ -1,11 +1,126 @@
 
+import java.io.*;
+import java.util.*;
+
 public class FileInputOutput {
-	String path;
-	public FileInputOutput(String Path) {
-		// TODO Auto-generated constructor stub
-		this.path = path;
+	
+	
+	// memulai dengan readFile
+	public static Matrix readFileMatrix() throws IOException{
+		boolean path_exist = false;
+		String path = "";
+		Scanner scan = new Scanner(System.in);
+		while (!path_exist) {
+			int pilihan;
+			do {
+				
+				System.out.println("====Proses membaca file====");
+				
+				System.out.println("1.Nama file (pastikan file di folder src)");
+				System.out.println("2.Full path");
+				System.out.print("Pilihan (1/2) : ");
+				pilihan = scan.nextInt();
+				scan.nextLine();
+				if (pilihan != 1 && pilihan != 2) {
+					System.out.println("Tolong masukkan pilihan yang benar");
+				}
+			}while (pilihan != 1 && pilihan != 2);
+			
+			if (pilihan == 1) {
+				path = System.getProperty("user.dir");
+				
+				System.out.print("Masukkan nama file (dengan extension) : ");
+				String file_name = scan.nextLine();
+				path += "\\" + "src\\" + file_name;
+				
+				
+			}
+			else if (pilihan == 2) {
+				System.out.print("Masukkan full path dengan benar : ");
+				path = scan.nextLine();
+			}
+			
+			
+			
+			File file = new File(path);
+			if (file.exists()) {
+				if (FileInputOutput.getColCountFile(path) == 0 || FileInputOutput.getRowCountFile(path) == 0) {
+					System.out.println("Matriks tidak ada/Tidak dalam posisi yang benar dalam file, tolong masukkan ulang nama file");
+				}
+				else {
+					path_exist = true;
+				}
+			}
+			else {
+				System.out.print("File tidak ada, tolong masukkan ulang nama file \n");
+			
+			}
+		}
+		scan.close();
+		return FileInputOutput.getMatrixFromFile(path);
+		
+		
 	}
-	public static void what() {
-		System.out.printf("Why hello");
+	
+	public static int getRowCountFile(String path) throws IOException{
+		int row_count = 0 ;
+		File file = new File(path);
+		Scanner scan_row = new Scanner(file);
+		while (scan_row.hasNextLine()) {
+			row_count++;
+			scan_row.nextLine();
+		}
+		scan_row.close();
+		
+		
+		return row_count;
 	}
+	
+	public static int getColCountFile(String path) throws IOException{
+		int col_count = 0;
+		File file = new File(path);
+		Scanner scan = new Scanner(file);
+		if (!scan.hasNextLine()) {
+			scan.close();
+			return 0;
+		}
+		Scanner scan_col = new Scanner(scan.nextLine());
+		while(scan_col.hasNextDouble()) {
+			col_count++;
+			scan_col.nextDouble();
+		}
+		scan.close();
+		scan_col.close();
+		
+		
+		return col_count;
+	}
+	
+	public static Matrix getMatrixFromFile(String path) throws IOException {
+		int row = 0 , col = 0 ;
+		int num_row  =getRowCountFile(path) ,num_col = getColCountFile(path);
+		Matrix mOut = new Matrix(num_row, num_col);
+		File file = new File(path);
+		Scanner scan = new Scanner(file);
+		while (scan.hasNextDouble()) {
+			mOut.setElmt(row, col, scan.nextDouble());
+			col++;
+			if (col >= num_col) {
+				col = 0;
+				row++;
+			}
+			if (row >= num_row) {
+				break;
+			}
+		}
+		scan.close();
+		
+		return mOut;
+		
+	}
+
+	
+	
+
+		
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.*;
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException  {
 		Scanner scan = new Scanner(System.in);
 //		// TODO Auto-generated method stub
 //		System.out.println("=======Percobaan gauss=========");
@@ -76,57 +78,71 @@ public class Main {
 ////		System.out.printf("k = %f\n",gauss2.K);
 ////		
 //		
-		System.out.println("==============Test Case==============");
-		// note asumsikan dalam matriks augmented (nanti diatur di UI), untuk test case dalam augmented dulu aja
-		System.out.print("Number of rows : ");
-		int row = scan.nextInt();
-		System.out.print("Number of columns : ");
-		int col = scan.nextInt();
-		Matrix mat = new Matrix(row,col);
-		mat.readMatrix();
+//		System.out.println("==============Test Case==============");
+//		// note asumsikan dalam matriks augmented (nanti diatur di UI), untuk test case dalam augmented dulu aja
+//		System.out.print("Number of rows : ");
+//		int row = scan.nextInt();
+//		System.out.print("Number of columns : ");
+//		int col = scan.nextInt();
+//		Matrix mat = new Matrix(row,col);
+//		mat.readMatrix();
+//		
+//		// hasil gauss
+//		Matrix hasil_gauss = SPL.gauss(mat);
+//		//hasil gauss jordan
+//		Matrix hasil_gauss_jordan = SPL.gaussjordan(mat);
+//		//hasil SPL dengan balikan
+//		Matrix splBalik =  SPL.splInverse(mat);
+//		
+//		//hasil cramer
+//		Matrix hasil_cramer = SPL.cramerMethod(mat);
+//		// Deterimnan reduksi
+//		double detOBE = Matrix.determinanReduksi(Matrix.getMatrixCoefficient(mat));
+//		// Determinan ekspansi kofaktor
+//		double detCofactor = Matrix.determinanEkspansiKofaktor(Matrix.getMatrixCoefficient(mat));
+//		
+//		//inverse dengan gauss
+//		Matrix inverse_gauss = Matrix.inversGaussJordan(Matrix.getMatrixCoefficient(mat));
+//		//inverse dengan adjoin
+//		Matrix inverse_adj = Matrix.getMatrixInverseAdjoin(Matrix.getMatrixCoefficient(mat));
+//		
+//		
+//		System.out.println("==============Percobaan gauss==============");
+//		hasil_gauss.displayMatrix();
+//		System.out.println("==============Percobaan gauss jordan==============");
+//		hasil_gauss_jordan.displayMatrix();
+//		System.out.println("==============Hasil parametric gauss ==============");
+//		DisplaySolution.displayGauss(hasil_gauss);
+//		System.out.println("==============Hasil parametric gauss jordan ==============");
+//		DisplaySolution.displayGaussJordan(hasil_gauss_jordan);
+//		System.out.println("============== Hasil dengan cremer (Hanya jika dia ada solusi (gaada parametric), kalo ga diisi marker) ==============");
+//		DisplaySolution.displayCramer(hasil_cramer);
+//		System.out.println("==============Hasil SPL dengan matriks balikan==============");
+//		DisplaySolution.displaySPLbalikan(splBalik);
+//		System.out.println("==============Hasil determinan reduksi (OBE)==============");
+//		System.out.printf("Determinan OBE = %f\n",detOBE);
+//		System.out.println("==============Hasil determinan ekspansi kofaktor ==============");
+//		System.out.printf("Determinan kofaktor = %f\n",detCofactor);
+//		System.out.println("==============Hasil inverse dengan gauss jordan ==============");
+//		inverse_gauss.displayMatrix();
+//		System.out.println("==============Hasil inverse dengan adjoin ==============");
+//		inverse_adj.displayMatrix();
+//		
+		System.out.println("==============Test read file ==============");
+		Matrix mat = FileInputOutput.readFileMatrix();
+		System.out.println("Hasil dari membaca file :");
+		mat.displayMatrix();
+		Matrix gauss = SPL.gauss(mat);
+		System.out.println("Hasil gauss :");
+		gauss.displayMatrix();
+		System.out.println("Hasil gauss jordan: ");
+		Matrix gauss_jordan = SPL.gaussjordan(mat);
+		gauss_jordan.displayMatrix();
+		System.out.println("Hasil parametric gauss: ");
+		DisplaySolution.displayGauss(gauss);
+		System.out.println("Hasil parametric gauss jordan: ");
+		DisplaySolution.displayGaussJordan(gauss_jordan);
 		
-		// hasil gauss
-		Matrix hasil_gauss = SPL.gauss(mat);
-		//hasil gauss jordan
-		Matrix hasil_gauss_jordan = SPL.gaussjordan(mat);
-		//hasil SPL dengan balikan
-		Matrix splBalik =  SPL.splInverse(mat);
-		
-		//hasil cramer
-		Matrix hasil_cramer = SPL.cramerMethod(mat);
-		// Deterimnan reduksi
-		double detOBE = Matrix.determinanReduksi(Matrix.getMatrixCoefficient(mat));
-		// Determinan ekspansi kofaktor
-		double detCofactor = Matrix.determinanEkspansiKofaktor(Matrix.getMatrixCoefficient(mat));
-		
-		//inverse dengan gauss
-		Matrix inverse_gauss = Matrix.inversGaussJordan(Matrix.getMatrixCoefficient(mat));
-		//inverse dengan adjoin
-		Matrix inverse_adj = Matrix.getMatrixInverseAdjoin(Matrix.getMatrixCoefficient(mat));
-		
-		
-		System.out.println("==============Percobaan gauss==============");
-		hasil_gauss.displayMatrix();
-		System.out.println("==============Percobaan gauss jordan==============");
-		hasil_gauss_jordan.displayMatrix();
-		System.out.println("==============Hasil parametric gauss ==============");
-		DisplaySolution.displayGauss(hasil_gauss);
-		System.out.println("==============Hasil parametric gauss jordan ==============");
-		DisplaySolution.displayGaussJordan(hasil_gauss_jordan);
-		System.out.println("============== Hasil dengan cremer (Hanya jika dia ada solusi (gaada parametric), kalo ga diisi marker) ==============");
-		DisplaySolution.displayCramer(hasil_cramer);
-		System.out.println("==============Hasil SPL dengan matriks balikan==============");
-		DisplaySolution.displaySPLbalikan(splBalik);
-		System.out.println("==============Hasil determinan reduksi (OBE)==============");
-		System.out.printf("Determinan OBE = %f\n",detOBE);
-		System.out.println("==============Hasil determinan ekspansi kofaktor ==============");
-		System.out.printf("Determinan kofaktor = %f\n",detCofactor);
-		System.out.println("==============Hasil inverse dengan gauss jordan ==============");
-		inverse_gauss.displayMatrix();
-		System.out.println("==============Hasil inverse dengan adjoin ==============");
-		inverse_adj.displayMatrix();
-		
-
 	}
 
 }
